2e885054b997cfdd0c810a21b323796dce4dfa57
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-25 21:23:47 -0400

    add no data case for adt spec

--- spec/monadt/adt_spec.rb
+++ spec/monadt/adt_spec.rb
@@ -3,7 +3,7 @@ require 'monadt/adt'
 module TestAdt
   One = data :foo, :bar
   Two = data :foo
-  Three = data :baz
+  Three = data
 end
 
 class UseAdts
@@ -12,14 +12,14 @@ class UseAdts
   def adt_func(o)
     match o,
       with(TestAdt::One, ->(foo, bar) { foo.to_s + bar.to_s }),
-      with(TestAdt::Three, ->(foo) { foo + 10 }),
+      with(TestAdt::Three, ->() { 10 }),
       with(Default, ->() { "default" })
   end
 
   def adt_func2(o)
     match o,
       with(TestAdt::One) { |foo, bar| foo.to_s + bar.to_s },
-      with(TestAdt::Three) { |foo| foo + 10 },
+      with(TestAdt::Three) { 10 },
       with(Default) { "default" }
   end
 end
@@ -27,19 +27,19 @@ end
 describe 'proc/block based ADTs' do
   let(:v1) { TestAdt::One.new 1, :five }
   let(:v2) { TestAdt::Two.new "hoi" }
-  let(:v3) { TestAdt::Three.new 6 }
+  let(:v3) { TestAdt::Three.new }
   let(:subject) { UseAdts.new }
 
   it 'supports procs' do
     expect(subject.adt_func(v1)).to eq("1five")
     expect(subject.adt_func(v2)).to eq("default")
-    expect(subject.adt_func(v3)).to eq(16)
+    expect(subject.adt_func(v3)).to eq(10)
   end
 
   it 'supports blocks' do
     expect(subject.adt_func2(v1)).to eq("1five")
     expect(subject.adt_func2(v2)).to eq("default")
-    expect(subject.adt_func2(v3)).to eq(16)
+    expect(subject.adt_func2(v3)).to eq(10)
   end
 end
 
