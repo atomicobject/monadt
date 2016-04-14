87351a81cc9800d9fb4df9207c6ecb105a33b444
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-04-14 12:32:36 -0400

    inefficient list

--- spec/monadt/monad_list_example.rb
+++ spec/monadt/monad_list_example.rb
@@ -34,61 +34,12 @@ end
 module Monadt
   class ListExample
     class << self
-
-      def map_param(param)
-        if param.is_a? Monadt::ListM
-          param.to_a
-        else
-          [param]
-        end
-      end
-
-      def lift_params(params)
-        if params.size == 0
-          nil
-        else
-          first = map_param params[0]
-          rest = params.slice(1..-1)
-          rest = lift_params rest
-          if rest.nil?
-            if first.is_a? Array
-              first.map do |x| [x] end
-            else
-              [first]
-            end
-          else
-            first.flat_map do |p0|
-              rest.map do |p1n|
-                [p0].concat p1n
-              end
-            end
-          end
-        end
-      end
-
-      def lift(prc, *params)
-        p_delta = lift_params(params)
-        ret = p_delta.flat_map do |ps|
-          prc.call(*ps)
-        end
-        ListM.create ret
-      end
-
       def list_func(v)
         lf = ListFuncs.new
         Monad.do_m(Monadt::ListM) do |m|
-          x = m.bind (lift lf.list1, v)
-          y = m.bind (lift lf.list2, x)
-          Monadt::ListM.return (lift lf.add, x, y)
-        end.to_a
-      end
-
-      def list_func_simple(v)
-        lf = ListFuncs.new
-        Monad.do_m(Monadt::ListM2) do |m|
           x = m.bind lf.list1(v)
           y = m.bind lf.list2(x)
-          Monadt::ListM2.return lf.add(x, y)
+          Monadt::ListM.return lf.add(x, y)
         end.to_a
       end
     end
--- spec/monadt/monad_list_spec.rb
+++ spec/monadt/monad_list_spec.rb
@@ -24,38 +24,4 @@ describe 'list monad' do
     expected = expected(lf, 10)
     expect(result).to eq(expected)
   end
-
-  it 'tries all the possibilities' do
-    lf = ListFuncs.new
-
-    result = Monadt::ListExample.list_func_simple(9)
-    expected = expected(lf, 9)
-    expect(result).to eq(expected)
-
-    result = Monadt::ListExample.list_func_simple(3)
-    expected = expected(lf, 3)
-    expect(result).to eq(expected)
-
-    result = Monadt::ListExample.list_func_simple(10)
-    expected = expected(lf, 10)
-    expect(result).to eq(expected)
-  end
-
-  def list(v)
-    Monadt::ListM.create v
-  end
-
-  it 'successfully lifts normal functions' do
-    lifted = Monadt::ListExample.lift_params([1,:two,"three"])
-    expect(lifted).to eq([[1,:two,"three"]])
-
-    lifted = Monadt::ListExample.lift_params([list([5,6,7])])
-    expect(lifted).to eq([[5], [6], [7]])
-
-    lifted = Monadt::ListExample.lift_params([1, list([:two, 2]), "three"])
-    expect(lifted).to eq([
-      [1, :two, "three"],
-      [1, 2, "three"]
-    ])
-  end
 end
